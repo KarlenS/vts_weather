29edec0be4a227cd3ef91c89cf072a1a7d33a274
KarlenS <shkarlen@gmail.com>
2017-05-21 23:39:37 -0500

    initial db access test version

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pymysql.cursors
-from datetime import date, datetime
+from astropy.time import Time
 
 
 class DBReader(object):
@@ -8,7 +8,7 @@ class DBReader(object):
 
     def __init__(self,db='VERITAS'):
         '''Initiates connection to a database. With current setup, each objects connects to a single database.
-        This means, a separate object is required for VERITAS and VOFFLINE databases.
+        This means, a separate object is required for VERITAS and VOFFLINE databases. Only have VERITAS anyways.
         '''
         self.client = pymysql.connect(host='lucifer1.spa.umn.edu',
                                      user='readonly',
@@ -28,18 +28,28 @@ class DBReader(object):
         return query_result
 
 
-    def getFIRInfo(self):
+    def getFIRInfo(self,date):
+
+        cmd =  "SELECT * FROM `tblFIR_Pyrometer_Info` ORDER BY ABS( DATEDIFF( timestamp, %s ) ) LIMIT 1" %(date)
+        fir_wthr = self.runQuery(cmd)
         return fir_wthr
 
     def getObserverWeather(self):
+        obs_withr = 0
         return obs_wthr
 
     def getWeatherSummary(self):
+        wthr_summary = 0
         return wthr_summary
 
 def main():
-    filename = 'HESSJ1943_Rerun/data-1/rep_photometry_r.dat'
-    dat = np.genfromtxt(filename, usecols=(1,2,3,4,5), names=['mjd','mag','refmag','magerr','refmagerr'],unpack=True)
+    filename = 'HESSJ1943_flux-r2/flux_photometry_r.dat'
+    dat = np.genfromtxt(filename, usecols=(1,2,3,4,5),skip_header=1, names=['mjd','flux','fluxerr','refflux','reffluxerr'],unpack=True)
+
+    dbr = DBReader()   
+
+    t = Time(55197., format='mjd')
+    dbr.getFIRInfo(t.iso)
     
 
 if __name__ == '__main__':
